e6628f0cbab411ec9272a770da639f8382e49fac
vishnu <vishnuchudhary2020@gmail.com>
2026-02-10 21:48:57 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import userRoutes from './routes/user.routes.js';
 
+
+app.use("/api/v1/users",userRoutes);
 
 
 
